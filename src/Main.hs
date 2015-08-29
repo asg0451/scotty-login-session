7d57c78e14fa9caa6658aa3c946f56b2d722e3bf
Miles Frankel <miles.frankel@gmail.com>
2015-08-29 00:43:16 -0400

    init

---
+++ src/Main.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+-- import           Web.Scotty.Trans         as S
+
+import           Web.Scotty               as S
+import           Web.Scotty.Cookie        as C
+
+import           Control.Applicative
+import           Control.Monad.IO.Class
+import           Control.Monad.State.Lazy as L
+import           Control.Monad.Trans
+import           Data.Functor
+import           Data.Monoid
+import qualified Data.Text                as TS
+import qualified Data.Text.IO             as TIO
+import qualified Data.Text.Lazy           as T
+
+
+import           Database.Persist         as D
+import           Database.Persist.Sqlite
+import           Model
+import           System.Random            (randomIO)
+
+main :: IO ()
+main = do
+  liftIO $ runDB $ runMigration migrateAll
+  scotty 4242 $ do
+    routes
+
+
+routes = do
+  S.get "/" $ S.text "home"
+  S.get "/login" $ do c <- liftIO $ readFile "static/login.html"
+                      S.html $ T.pack $ c
+  S.post "/login" $ do
+    h <- liftIO $ (randomIO :: IO Int)
+    let val = TS.pack $ show h
+    setSimpleCookie "SessionId" val
+    liftIO $ insertSession $ T.fromStrict val
+    redirect "/"
+  S.get "/authed" $ do
+    c <- getCookie "SessionId"
+    case c of
+     Nothing -> S.text "auth cookie not found"
+     Just v -> do -- Text
+       session <- liftIO $ runDB $ selectFirst [SessionSid ==. T.fromStrict v] []
+       case session of
+        Nothing -> S.text "auth cookie found but not in db"
+        Just s -> do
+          S.text "authed"
+
+
+
+insertSession sid = runDB $ insert $ Session sid
+
+runDB = runSqlite ":memory:" -- "db.sqlite3"
+
+
+
+-- type Env = [(String, String)]
+-- type StateIO = StateT Env IO -- still takes ret val
+
+-- type ActionM' = ActionT T.Text StateIO
+
+-- s = [("a", "30")] :: Env
+
+-- main = do
+--   scottyT 4242 (`evalStateT` s) $ do
+--     S.get "/" $ app'
+
+-- app' :: ActionM' ()
+-- app' =  do
+--   v <- lift $ L.get
+--   html $ T.pack $ show $ v
---
+++ src/Model.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
+{-# OPTIONS_GHC -fno-warn-orphans       #-}
+
+module Model where
+
+-- import Database.Persist
+import qualified Data.Text.Lazy      as T
+import           Database.Persist.TH
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
+   Session
+           sid T.Text
+           deriving Show
+  |]
