2bcf2779bcbc70fa561e1092b51d0e5918700ebe
Miles Frankel <miles.frankel@gmail.com>
2015-08-30 21:31:59 -0400

    added configurability

--- src/Test.hs
+++ src/Test.hs
@@ -3,29 +3,35 @@
 
 module Main where
 
+import           Control.Monad.IO.Class   (liftIO)
 import qualified Data.Text.Lazy           as T
 import           Web.Scotty               as S
 import           Web.Scotty.Login.Session
 
-import           Control.Monad.IO.Class   (liftIO)
+conf :: SessionConfig
+conf = defaultSessionConfig
 
 main :: IO ()
 main = do
-  initializeCookieDb
-  scotty (4040) routes
+  initializeCookieDb conf
+  scotty 4040 routes
 
 routes :: ScottyM ()
 routes = do
   S.get "/" $ S.text "home"
   S.get "/denied" $ S.text "login denied -- wrong username or password"
-  S.get "/login" $ do c <- liftIO $ readFile "static/login.html"
-                      S.html $ T.pack c
+  S.get "/login" $ do S.html $ T.pack $ unlines $
+                        ["<form method=\"POST\" action=\"/login\">"
+                        , "<input type=\"text\" name=\"username\">"
+                        , "<input type=\"password\" name=\"password\">"
+                        , "<input type=\"submit\" name=\"login\" value=\"login\">"
+                        , "</form>"]
   S.post "/login" $ do
     (usn :: String) <- param "username"
     (pass :: String) <- param "password"
     if usn == "guest" && pass == "password"
-      then do addSession
+      then do addSession conf
               redirect "/authed"
       else do redirect "/denied"
-  S.get "/authed" $ authCheck (redirect "/denied") $
+  S.get "/authed" $ authCheck conf (redirect "/denied") $
     S.text "authed"
--- src/Web/Scotty/Login/Session.hs
+++ src/Web/Scotty/Login/Session.hs
@@ -1,60 +1,80 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
 
 module Web.Scotty.Login.Session ( initializeCookieDb
                                 , addSession
                                 , authCheck
+                                , SessionConfig(..)
+                                , defaultSessionConfig
                                 )
        where
-
-import           Control.Concurrent
+import           Control.Concurrent                (forkIO, threadDelay)
 import           Control.Monad.IO.Class
 import qualified Data.Text                         as TS
 import qualified Data.Text.Lazy                    as T
 import           Data.Time.Clock
 import           Database.Persist                  as D
 import           Database.Persist.Sqlite
-import           System.Random                     (randomIO)
 import           Web.Scotty.Cookie                 as SC
 import           Web.Scotty.Trans                  as S
+
+import           Crypto.Random                     (getRandomBytes)
+import qualified Data.ByteString                   as B
+import           Numeric                           (showHex)
 
 import           Web.Scotty.Login.Internal.Cookies as C
 import           Web.Scotty.Login.Internal.Model
 
-initializeCookieDb :: IO ()
-initializeCookieDb = void $ do
-  runDB $ runMigration migrateAll
-  forkIO dbEraseLoop -- erase expired sessions
+import           Control.Monad.Logger              (NoLoggingT)
+import           Control.Monad.Trans.Resource      (ResourceT)
+
+data SessionConfig = SessionConfig { dbName             :: String
+                                   , cleanupInterval    :: Int --seconds
+                                   , expirationInterval :: NominalDiffTime
+                                   }
 
 
-dbEraseLoop :: IO ()
-dbEraseLoop = do threadDelay 60000000 -- 1 minute
-                 print "deleting expired sessionids..."
-                 t <- getCurrentTime
-                 runDB $ deleteWhere [SessionExpiration <=. t]
-                 dbEraseLoop -- recurse
 
+defaultSessionConfig :: SessionConfig
+defaultSessionConfig = SessionConfig "sessions.sqlite3" 60 120
 
-addSession ::  ActionT T.Text IO (Key Session)
-addSession = do
-  h <- liftIO (randomIO :: IO Int)
+initializeCookieDb :: SessionConfig -> IO ()
+initializeCookieDb c = void $ do
+  runDB c $ runMigration migrateAll
+  forkIO $ dbEraseLoop c -- erase expired sessions
+
+dbEraseLoop :: SessionConfig -> IO ()
+dbEraseLoop c = do threadDelay $ cleanupInterval c * 1000000
+                   putStrLn "Deleting expired sessions from database..."
+                   t <- getCurrentTime
+                   runDB c $ deleteWhere [SessionExpiration <=. t]
+                   dbEraseLoop c -- recurse
+
+addSession :: SessionConfig -> ActionT T.Text IO (Key Session)
+addSession c = do
+  (bh :: B.ByteString) <- liftIO $ getRandomBytes 128
+  let bs = map (`showHex` "") $ B.unpack bh
+      h = mconcat bs
   t <- liftIO getCurrentTime
-  let t' = addUTCTime 600 t -- ten minutes
-      val = TS.pack $ show h
+  let t' = addUTCTime (expirationInterval c) t -- ten minutes
+      val = TS.pack h
   C.setSimpleCookieExpr "SessionId" val t'
-  liftIO $ insertSession (T.fromStrict val) t'
+  liftIO $ insertSession c (T.fromStrict val) t'
 
 -- denial action, approval action
 authCheck :: (MonadIO m, ScottyError e)
-             => ActionT e m ()
+             => SessionConfig
              -> ActionT e m ()
              -> ActionT e m ()
-authCheck d a = do
+             -> ActionT e m ()
+authCheck conf d a = do
   c <- SC.getCookie "SessionId"
   case c of
    Nothing -> d
    Just v -> do -- Text
-     session <- liftIO $ runDB $ selectFirst [SessionSid ==. T.fromStrict v] []
+     session <- liftIO $ runDB conf $ selectFirst [SessionSid ==. T.fromStrict v] []
      case session of
       Nothing -> d
       Just e -> do let s = entityVal e
@@ -65,10 +85,13 @@ authCheck d a = do
                      else d -- this shouldnt happen, browser should delete it
 
 
-insertSession :: T.Text -> UTCTime -> IO (Key Session)
-insertSession sid t = runDB $ insert $ Session sid t
+insertSession :: SessionConfig -> T.Text -> UTCTime -> IO (Key Session)
+insertSession c sid t = runDB c $ insert $ Session sid t
 
-runDB = runSqlite "sessions.sqlite3"
+runDB :: SessionConfig
+         -> SqlPersistT (NoLoggingT (ResourceT IO)) a
+         -> IO a
+runDB c = runSqlite $ TS.pack $ dbName c
 
 void :: (Monad m) => m a -> m ()
 void a = a >> return ()
