ec01696e5a892125871c61996fd40ddc047e5aae
Miles Frankel <miles.frankel@gmail.com>
2015-09-21 22:47:37 -0400

    example in doc

--- src/Web/Scotty/Login/Session.hs
+++ src/Web/Scotty/Login/Session.hs
@@ -12,8 +12,45 @@ License:             GPL-2
 
 A Simple library for session adding and checking, with automatic SQLite backup of session store. The store is stored in memory for fast access. Session cookie expiration and database syncing timing are configurable.
 
--}
+Example usage:
 
+@
+\{\-\# LANGUAGE OverloadedStrings   \#\-\}
+\{\-\# LANGUAGE ScopedTypeVariables \#\-\}
+
+module Main where
+import qualified Data.Text.Lazy           as T
+import           Web.Scotty               as S
+import           Web.Scotty.Login.Session
+
+conf :: SessionConfig
+conf = defaultSessionConfig
+
+main :: IO ()
+main = do
+  initializeCookieDb conf
+  scotty 4040 routes
+
+routes :: ScottyM ()
+routes = do
+  S.get \"/denied\" $ S.text \"login denied -- wrong username or password\"
+  S.get \"/login\" $ do S.html $ T.pack $ unlines $
+                        [ \"\<form method=\\\"POST\\\" action=\\\"/login\\\"\>\"
+                        , \"\<input type=\\\"text\\\" name=\\\"username\\\"\>\"
+                        , \"\<input type=\\\"password\\\" name=\\\"password\\\"\>\"
+                        , \"\<input type=\\\"submit\\\" name=\\\"login\\\" value=\\\"login\\\"\>\"
+                        , \"\</form\>\" ]
+  S.post \"/login\" $ do
+    (usn :: String) <- param \"username\"
+    (pass :: String) <- param \"password\"
+    if usn == \"guest\" && pass == \"password\"
+      then do addSession conf
+              redirect \"/authed\"
+      else do redirect \"/denied\"
+  S.get \"\/authcheck\" $ authCheck (redirect \"\/denied\") $
+    S.text \"authorized\"
+@
+-}
 
 module Web.Scotty.Login.Session ( initializeCookieDb
                                 , addSession
